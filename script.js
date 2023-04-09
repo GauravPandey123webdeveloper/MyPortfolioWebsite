62f079b9d9fa8450503a1445af22fa50a0c6dd76
GauravPandey123webdeveloper <130306260+GauravPandey123webdeveloper@users.noreply.github.com>
2023-04-09 22:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function effect(){
+    document.getElementById("change").style.backgroundColor=" #f4a37a"
+    }
+    function default_color(){
+        document.getElementById("change").style.backgroundColor=" #eff0ea"
+    }
